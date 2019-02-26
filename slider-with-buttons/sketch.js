ad3f4a207ec7b0543a79e45da804e593f0ea2fa9
Вячеслав Романьков <46442686+vrom1990@users.noreply.github.com>
2019-02-26 13:15:13 +0700

    Update sketch.js

--- slider-with-buttons/sketch.js
+++ slider-with-buttons/sketch.js
@@ -16,10 +16,10 @@ function preload(){
 function setup(){
   createCanvas(400,400);
 	button1 = createButton('<<');
-  button1.position(200, 420);
+  button1.position(160, 420);
   button1.mousePressed(Prev);
 	button2 = createButton('>>');
-  button2.position(250, 420);
+  button2.position(160, 420);
   button2.mousePressed(Next);
 	textSize(12);
 	textFont(OpenSans);
