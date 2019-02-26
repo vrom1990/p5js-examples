e8f7829482a350f2f0516458f41c39dd7ed22e0f
Вячеслав Романьков <46442686+vrom1990@users.noreply.github.com>
2019-02-26 13:07:42 +0700

    Update sketch.js

--- slider-with-buttons/sketch.js
+++ slider-with-buttons/sketch.js
@@ -1,27 +1,36 @@
 let img = [];
-let index = 0;
-let button1, button2;
-function preload(){
+let txt = [];
+var index = 0;
+
  
+function preload(){
+ 	OpenSans = loadFont('opensans.ttf');
   img[0] = loadImage("I82e0eMHBAA.jpg");
-  img[1] =  loadImage("1FqdWjJaqQE.jpg");
-  img[2] =  loadImage("andandand.jpg");
+  img[1] = loadImage("1FqdWjJaqQE.jpg");
+	img[2] = loadImage("andandand.jpg");
+	txt[0] = "Слайд 1";
+	txt[1] = "Слайд 2";
+	txt[2] = "Слайд 3";
 }
  
 function setup(){
   createCanvas(400,400);
-  button1 = createButton('<<');
-  button1.position(200, 450);
+	button1 = createButton('<<');
+  button1.position(200, 395);
   button1.mousePressed(Prev);
-  button2 = createButton('>>');
-  button2.position(250, 450);
+	button2 = createButton('>>');
+  button2.position(250, 395);
   button2.mousePressed(Next);
+	textSize(12);
+	textFont(OpenSans);
 }
  
 function draw(){ 
-  image(img[index],50,50,width,height);
+  image(img[index],0,0,width-50,height-50);
+	fill(0);
+  text(txt[index], 200, 375);
 }
-
+ 
 function Prev(){
 	if (index === 0){
 		index = 2;
