f0df69f6e4dcfcc2c52d6daa38efcaa0eb57fae3
Вячеслав Романьков <46442686+vrom1990@users.noreply.github.com>
2019-02-26 13:16:52 +0700

    Update sketch.js

--- slider-with-buttons/sketch.js
+++ slider-with-buttons/sketch.js
@@ -4,13 +4,13 @@ var index = 0;
 
  
 function preload(){
- 	OpenSans = loadFont('opensans.ttf');
+  OpenSans = loadFont('opensans.ttf');
   img[0] = loadImage("I82e0eMHBAA.jpg");
   img[1] = loadImage("1FqdWjJaqQE.jpg");
-	img[2] = loadImage("andandand.jpg");
-	txt[0] = "Слайд 1";
-	txt[1] = "Слайд 2";
-	txt[2] = "Слайд 3";
+  img[2] = loadImage("andandand.jpg");
+  txt[0] = "Слайд 1";
+  txt[1] = "Слайд 2";
+  txt[2] = "Слайд 3";
 }
  
 function setup(){
@@ -18,19 +18,19 @@ function setup(){
 	button1 = createButton('<<');
   button1.position(160, 420);
   button1.mousePressed(Prev);
-	button2 = createButton('>>');
+  button2 = createButton('>>');
   button2.position(160, 420);
   button2.mousePressed(Next);
-	textSize(12);
-	textFont(OpenSans);
-	textAlign(CENTER);
+  textSize(12);
+  textFont(OpenSans);
+  textAlign(CENTER);
 }
  
 function draw(){ 
-background(255);
+  background(255);
   image(img[index],0,0,width-50,height-50);
-	fill(0);
-  text(txt[index], 200, 375);
+  fill(0);
+  text(txt[index], 200, 370);
 }
  
 function Prev(){
