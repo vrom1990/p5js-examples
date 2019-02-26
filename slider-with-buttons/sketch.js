551e9175605867364ebea03b62c47bc4d705966d
Вячеслав Романьков <46442686+vrom1990@users.noreply.github.com>
2019-02-26 11:00:31 +0700

    Create sketch.js

---
+++ slider-with-buttons/sketch.js
@@ -0,0 +1,41 @@
+let img = [];
+let index = 0;
+let button1, button2;
+function preload(){
+ 
+  img[0] = loadImage("I82e0eMHBAA.jpg");
+  img[1] =  loadImage("1FqdWjJaqQE.jpg");
+	img[2] =  loadImage("andandand.jpg");
+}
+ 
+function setup(){
+  createCanvas(400,400);
+	button1 = createButton('<<');
+  button1.position(200, 65);
+  button1.mousePressed(Prev);
+	button2 = createButton('>>');
+  button2.position(250, 65);
+  button2.mousePressed(Next);
+}
+ 
+function draw(){ 
+  image(img[index],50,50,500,500);
+}
+ 
+function Prev(){
+	if (index === 0){
+		index = 2;
+	}
+	else{
+		index = index +1;	
+	}
+}
+
+function Next(){
+	if (index === 2){
+		index = 0;
+	}
+	else{
+		index = index - 1;	
+	}
+}
